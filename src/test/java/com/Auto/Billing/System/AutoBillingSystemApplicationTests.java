8b185abc659d1bf49f6a61cfccf6fcc6ec51537e
Dhruv Ranjan <dhruvranjansharma@gmail.com>
2025-08-01 23:37:11 +0530

    Initial Backend Code

---
+++ src/test/java/com/Auto/Billing/System/AutoBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auto.Billing.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
